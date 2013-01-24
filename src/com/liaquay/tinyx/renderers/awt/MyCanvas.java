980f6df91a8577661b418dfa7315fbb67d0f51f1
ncludki <nludkin@googlemail.com>
2013-01-24 20:42:40 +0000

    Missed the MyCanvas (Overrides the clear) from the last checkin

---
+++ src/com/liaquay/tinyx/renderers/awt/MyCanvas.java
@@ -0,0 +1,14 @@
+package com.liaquay.tinyx.renderers.awt;
+
+import java.awt.Canvas;
+import java.awt.Graphics;
+
+public class MyCanvas extends Canvas {
+
+	@Override
+	public void paint(Graphics g) {
+		
+	}
+
+
+}
